91f20a788179839e9669d944537028bd93ba2707
Ridefishcat <zz12381@outlook.com>
2019-06-01 20:48:06 +0800

    third

---
+++ pinyougou-parent/pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Specification.java
@@ -0,0 +1,39 @@
+package com.pinyougou.pojogroup;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.pinyougou.pojo.TbSpecification;
+import com.pinyougou.pojo.TbSpecificationOption;
+
+/**
+ * 规格组合实体类，实现保存功能
+ * 前端传过来的数据格式{Specification:{},specificationOptionList:[{},{}...]}
+ * @author 张哲
+ *
+ */
+public class Specification implements Serializable{
+	
+	private TbSpecification specification;
+	
+	private List<TbSpecificationOption> specificationOptionList;
+
+	public TbSpecification getSpecification() {
+		return specification;
+	}
+
+	public void setSpecification(TbSpecification specification) {
+		this.specification = specification;
+	}
+
+	public List<TbSpecificationOption> getSpecificationOptionList() {
+		return specificationOptionList;
+	}
+
+	public void setSpecificationOptionList(List<TbSpecificationOption> specificationOptionList) {
+		this.specificationOptionList = specificationOptionList;
+	}
+	
+	
+
+}
